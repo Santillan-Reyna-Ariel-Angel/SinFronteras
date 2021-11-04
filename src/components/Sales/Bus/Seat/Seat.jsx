f9288f823a8df1904ea1587aca3d2f509508352d
Santillan Reyna Ariel Angel <santillanreynaarielangel1@gmail.com>
2021-11-04 19:20:02 -0400

    078_LimpiandoCodigo

--- src/components/Sales/Bus/Seat/Seat.jsx
+++ src/components/Sales/Bus/Seat/Seat.jsx
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from "react";
-import EventSeatRoundedIcon from "@mui/icons-material/EventSeatRounded";
-import Tooltip from "@mui/material/Tooltip";
-import { Background } from "./SeatStyles";
+import React, { useState } from 'react';
+import EventSeatRoundedIcon from '@mui/icons-material/EventSeatRounded';
+import Tooltip from '@mui/material/Tooltip';
+import { Background } from './SeatStyles';
 //check
-import Checkbox from "@mui/material/Checkbox";
+import Checkbox from '@mui/material/Checkbox';
 
-const label = { inputProps: { "aria-label": "Checkbox demo" } };
-
-const seatsList = [];
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 const Seat = () => {
   const [cant, setCant] = useState(27);
@@ -18,32 +16,6 @@ const Seat = () => {
       itemNumber[i] = i + 1;
     }
   };
-  //   const SeatsBus = () => {
-  //     let seatId = [];
-  //     for (let i = 0; i < cant; i++) {
-  //       seatId[i] = i + 1;
-  //       let seatsData = {
-  //         typeSeat: "cama",
-  //         id: seatId[i],
-  //         status: true,
-  //       };
-  //       seatsList.push(seatsData);
-  //     }
-  //   };
-
-  //   useEffect(() => {
-  //     SeatsBus();
-  //   }, [cant]);
-  //   const busMap = () => {
-  //     for (let i = 1; i <= cant; i++) {
-  //       let seatsData = {
-  //         typeSeat: "cama",
-  //         id: i,
-  //         status: true,
-  //       };
-  //       seatsList.push(seatsData);
-  //     }
-  //   };
 
   return (
     <>
@@ -55,10 +27,10 @@ const Seat = () => {
             <Tooltip
               title={
                 <>
-                  <p>Tipo: {"cama"}</p>
+                  <p>Tipo: {'cama'}</p>
                   <p>N#: {itemId}</p>
-                  <p>Precio Rango: {"80bs-90bs"}</p>
-                  <p>Estado: {true ? "Libre" : "Ocupado"}</p>
+                  <p>Precio Rango: {'80bs-90bs'}</p>
+                  <p>Estado: {true ? 'Libre' : 'Ocupado'}</p>
                 </>
               }
               arrow
