0b548a46c2f298c75139e67b0fd04c4db9d36ae9
Santillan Reyna Ariel Angel <santillanreynaarielangel1@gmail.com>
2021-11-23 20:26:28 -0400

    096_GeneracionDeAsientos_3Y4Filas

--- src/components/Sales/Bus/Seat/Seat.jsx
+++ src/components/Sales/Bus/Seat/Seat.jsx
@@ -2,7 +2,14 @@ import React, { useState, useContext } from 'react';
 import EventSeatRoundedIcon from '@mui/icons-material/EventSeatRounded';
 import Tooltip from '@mui/material/Tooltip';
 //Estilos
-import { Background, Container, ContainerTopSeats, ContainerCenterSeats, ContainerButtomSeats,ContainerHall } from './SeatStyles';
+import {
+  Background,
+  Container,
+  ContainerTopSeats,
+  ContainerCenterSeats,
+  ContainerButtomSeats,
+  ContainerHall,
+} from './SeatStyles';
 //check
 import Checkbox from '@mui/material/Checkbox';
 //form controls
@@ -10,24 +17,85 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 //Context
 import { ContextBranchOffice } from './../../../../contexts/ContextBranchOffice';
 
-
 //Usado en Check
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 //Algoritmo que genera 3 filas
-let createSeats = (numSeat) => {
+let createSeats = (typeOfBus, numSeat) => {
   let seats = [];
-  for(let i=1; i<=numSeat; i++){
-      if(i%3===0){
-          seats.push([i-2,i-1,i]);
+  for (let i = 1; i <= numSeat; i++) {
+    if (typeOfBus === 'normal') {
+      if (i % 4 === 0) {
+        seats.push([i - 3, i - 2, i - 1, i]);
       }
+    }
+    if ((typeOfBus = 'leito')) {
+      if (i % 3 === 0) {
+        seats.push([i - 2, i - 1, i]);
+      }
+    }
   }
-  if(numSeat%3===0)return [seats.map(x=>x[0]),seats.map(x=>x[1]),seats.map(x=>x[2])];
-  else{ 
-      let f = numSeat - seats.length*3;
-      seats.push(f<2?[numSeat,null,null]:[numSeat-1,numSeat,null]);
-      return [seats.map(x=>x[0]),seats.map(x=>x[1]),seats.map(x=>x[2])];
+
+  if (typeOfBus === 'normal') {
+    if (numSeat % 4 === 0)
+      return [
+        seats.map((x) => x[0]),
+        seats.map((x) => x[1]),
+        seats.map((x) => x[2]),
+        seats.map((x) => x[3]),
+      ];
+    else {
+      let f = numSeat - seats.length * 4;
+      seats.push(
+        f === 1
+          ? [numSeat, null, null, null]
+          : f === 2
+          ? [numSeat - 1, numSeat, null, null]
+          : [numSeat - 2, numSeat - 1, numSeat, null]
+      );
+      return [
+        seats.map((x) => x[0]),
+        seats.map((x) => x[1]),
+        seats.map((x) => x[2]),
+        seats.map((x) => x[3]),
+      ];
+    }
   }
-}
+
+  if (typeOfBus === 'leito') {
+    if (numSeat % 3 === 0)
+      return [
+        seats.map((x) => x[0]),
+        seats.map((x) => x[1]),
+        seats.map((x) => x[2]),
+      ];
+    else {
+      let f = numSeat - seats.length * 3;
+      seats.push(f < 2 ? [numSeat, null, null] : [numSeat - 1, numSeat, null]);
+      return [
+        seats.map((x) => x[0]),
+        seats.map((x) => x[1]),
+        seats.map((x) => x[2]),
+      ];
+    }
+  }
+
+  //
+  // if (numSeat % 3 === 0)
+  //   return [
+  //     seats.map((x) => x[0]),
+  //     seats.map((x) => x[1]),
+  //     seats.map((x) => x[2]),
+  //   ];
+  // else {
+  //   let f = numSeat - seats.length * 3;
+  //   seats.push(f < 2 ? [numSeat, null, null] : [numSeat - 1, numSeat, null]);
+  //   return [
+  //     seats.map((x) => x[0]),
+  //     seats.map((x) => x[1]),
+  //     seats.map((x) => x[2]),
+  //   ];
+  // }
+};
 
 const Seat = ({ dataBusTravel }) => {
   //Context
@@ -82,66 +150,103 @@ const Seat = ({ dataBusTravel }) => {
   };
   // console.log('checked', checked);
 
-  const BusMap=(numberOfSeatsParameter,indice)=>{
-    return createSeats(numberOfSeatsParameter)[indice].map((seatNumber) => {
-      if(seatNumber!==null){
-        return (
-          <Tooltip
-            key={seatNumber}
-            title={
-              <>
-                <p>N#: {seatNumber}</p>
-                <p>Tipo: {typeOfSeats}</p>
-                <p>
-                  Precio Rango:
-                  {` ${seatData.minimalPrice} bs - ${seatData.maximumPrice} bs`}
-                </p>
-                <p>Estado: {true ? 'Libre' : 'Ocupado'}</p>
-              </>
-            }
-            arrow
-            placement="top"
-          >
-            <FormControlLabel
-              control={
-                <Checkbox
-                  {...label}
-                  icon={
-                    <EventSeatRoundedIcon
-                      // fontSize="small"
-                      sx={{ transform: 'rotate(90deg)' }}
-                    />
-                  }
-                  checkedIcon={
-                    <EventSeatRoundedIcon
-                      // fontSize="small"
-                      sx={{ transform: 'rotate(90deg)' }}
-                    />
-                  }
-                  onChange={handleChange}
-                  sx={{ margin: '-16px 0px 0px 0px' }}
-                />
+  const BusMap = (typeOfBusParameter, numberOfSeatsParameter, indice) => {
+    return createSeats(typeOfBusParameter, numberOfSeatsParameter)[indice].map(
+      (seatNumber) => {
+        if (seatNumber !== null) {
+          return (
+            <Tooltip
+              key={seatNumber}
+              title={
+                <>
+                  <p>N#: {seatNumber}</p>
+                  <p>Tipo: {typeOfSeats}</p>
+                  <p>
+                    Precio Rango:
+                    {` ${seatData.minimalPrice} bs - ${seatData.maximumPrice} bs`}
+                  </p>
+                  <p>Estado: {true ? 'Libre' : 'Ocupado'}</p>
+                </>
               }
-              label={seatNumber}
-              labelPlacement="top"
-              sx={{ margin: '0px -12px -10px 0px' }}
-            />
-          </Tooltip>
-        );
-      }else{
-        return null;
+              arrow
+              placement="top"
+            >
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    {...label}
+                    icon={
+                      <EventSeatRoundedIcon
+                        // fontSize="small"
+                        sx={{ transform: 'rotate(90deg)' }}
+                      />
+                    }
+                    checkedIcon={
+                      <EventSeatRoundedIcon
+                        // fontSize="small"
+                        sx={{ transform: 'rotate(90deg)' }}
+                      />
+                    }
+                    onChange={handleChange}
+                    sx={{ margin: '-16px 0px 0px 0px' }}
+                  />
+                }
+                label={seatNumber}
+                labelPlacement="top"
+                sx={{ margin: '0px -12px -10px 0px' }}
+              />
+            </Tooltip>
+          );
+        } else {
+          return null;
+        }
       }
-      })
-  }
-  
+    );
+  };
+
   return (
     <>
       <Background>
         <Container>
-          <ContainerTopSeats>{BusMap(numberOfSeats,2)}</ContainerTopSeats>
+          {typeOfBus === 'normal' ? (
+            <>
+              <ContainerTopSeats>
+                {BusMap(typeOfBus, numberOfSeats, 2)}
+              </ContainerTopSeats>
+              <ContainerHall></ContainerHall>
+              <ContainerCenterSeats>
+                {BusMap(typeOfBus, numberOfSeats, 1)}
+              </ContainerCenterSeats>
+              <ContainerButtomSeats>
+                {BusMap(typeOfBus, numberOfSeats, 0)}
+              </ContainerButtomSeats>
+            </>
+          ) : (
+            <>
+              <ContainerTopSeats>
+                {BusMap(typeOfBus, numberOfSeats, 3)}
+              </ContainerTopSeats>
+              <ContainerCenterSeats>
+                {BusMap(typeOfBus, numberOfSeats, 2)}
+              </ContainerCenterSeats>
+              <ContainerHall></ContainerHall>
+              <ContainerCenterSeats>
+                {BusMap(typeOfBus, numberOfSeats, 1)}
+              </ContainerCenterSeats>
+              <ContainerButtomSeats>
+                {BusMap(typeOfBus, numberOfSeats, 0)}
+              </ContainerButtomSeats>
+            </>
+          )}
+
+          {/* <ContainerTopSeats>{BusMap(numberOfSeats, 2)}</ContainerTopSeats>
           <ContainerHall></ContainerHall>
-          <ContainerCenterSeats>{BusMap(numberOfSeats,1)}</ContainerCenterSeats>
-          <ContainerButtomSeats>{BusMap(numberOfSeats,0)}</ContainerButtomSeats>
+          <ContainerCenterSeats>
+            {BusMap(numberOfSeats, 1)}
+          </ContainerCenterSeats>
+          <ContainerButtomSeats>
+            {BusMap(numberOfSeats, 0)}
+          </ContainerButtomSeats> */}
         </Container>
       </Background>
     </>
