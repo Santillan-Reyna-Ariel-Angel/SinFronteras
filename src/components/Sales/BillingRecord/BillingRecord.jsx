1fb9d2f7da8edf0b4b58862642f777d14e6b1d76
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2022-05-03 09:56:26 -0400

    119_Componente_BillingRecord_V2

--- src/components/Sales/BillingRecord/BillingRecord.jsx
+++ src/components/Sales/BillingRecord/BillingRecord.jsx
@@ -12,6 +12,68 @@ const BillingRecord = ({ rowsState }) => {
     setNewCiOrNit(event.target.value);
   };
 
+  //Nuevo:
+  let cisOrNits = rowsState.map((passenger) => {
+    return passenger.identificationNumber;
+  });
+
+  let completeNamesAux = rowsState.map((passenger) => {
+    if (passenger.identificationNumber === newCiOrNit) {
+      return `${passenger.lastName} ${passenger.firstName}`;
+    } else {
+      return 'vacio';
+    }
+  });
+  let completeNames = completeNamesAux.filter(
+    (passenger) => passenger !== 'vacio'
+  );
+
+  //revisar: https://mui.com/x/react-data-grid/filtering/#main-content
+  // import * as React from 'react';
+  // import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+  // import { useDemoData } from '@mui/x-data-grid-generator';
+
+  // const VISIBLE_FIELDS = [
+  //   'name',
+  //   'rating',
+  //   'country',
+  //   'dateCreated',
+  //   'isAdmin',
+  // ];
+
+  // export default function BasicExampleDataGrid() {
+  //   const { data } = useDemoData({
+  //     dataSet: "Employee",
+  //     visibleFields: VISIBLE_FIELDS,
+  //     rowLength: 100
+  //   });
+  //   // {value: "ST",code: "ST",label: "Sao Tome and Principe",phone: "239"}
+  //   console.log(data.rows[0].country);
+  //   return (
+  //     <div style={{ height: 400, width: "100%" }}>
+  //       <DataGrid {...data} components={{ Toolbar: GridToolbar }} />
+  //     </div>
+  //   );
+  // }
+
+  let countryCodes = ['+591', '+1'];
+
+  const [billingContactInformation, setBillingContactInformation] = useState({
+    ciOrNit: '',
+    nameOrSocialReason: '',
+    email: '',
+    countryCode: '',
+    mobile: '',
+  });
+  console.log('billingContactInformation: ', billingContactInformation);
+
+  const handleInputChange2 = (event) => {
+    setBillingContactInformation({
+      ...billingContactInformation,
+      [event.target.name]: event.target.value.toLowerCase(),
+    });
+  };
+  // --------------------------------------------------
   //inputNameOrSocialReason:
   let fullNameAux = rowsState.map((passenger) => {
     if (passenger.identificationNumber === newCiOrNit) {
@@ -63,6 +125,88 @@ const BillingRecord = ({ rowsState }) => {
             onChange={nameOrSocialReason}
           />
         )}
+      />
+
+      {/* Nuevo: */}
+      <Autocomplete
+        value={billingContactInformation.ciOrNit}
+        onChange={(event, newValue) => {
+          setBillingContactInformation(newValue);
+        }}
+        id="ciOrNit"
+        options={cisOrNits}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            className="input"
+            type="number"
+            variant="outlined"
+            label="CI/NIT:"
+            name="ciOrNit"
+            value={billingContactInformation.ciOrNit}
+            onChange={handleInputChange2}
+          />
+        )}
+      />
+      <Autocomplete
+        value={billingContactInformation.nameOrSocialReason}
+        onChange={(event, newValue) => {
+          setBillingContactInformation(newValue);
+        }}
+        id="nameOrSocialReason"
+        options={completeNames}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            className="input"
+            type="text"
+            variant="outlined"
+            label="Nombre/Rason Social:"
+            name="nameOrSocialReason"
+            value={billingContactInformation.nameOrSocialReason}
+            onChange={handleInputChange2}
+          />
+        )}
+      />
+      <TextField
+        className="input"
+        type="email"
+        variant="outlined"
+        label="Correo:"
+        name="email"
+        value={billingContactInformation.email}
+        onChange={handleInputChange2}
+      />
+
+      <Autocomplete
+        value={billingContactInformation.countryCode}
+        onChange={(event, newValue) => {
+          setBillingContactInformation(newValue);
+        }}
+        id="countryCode"
+        options={countryCodes}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            className="input"
+            type="number"
+            variant="outlined"
+            label="Codigo pais:"
+            name="countryCode"
+            value={billingContactInformation.countryCode}
+            onChange={handleInputChange2}
+          />
+        )}
+      />
+
+      <TextField
+        className="input"
+        type="number"
+        variant="outlined"
+        label="Celular:"
+        name="mobile"
+        value={billingContactInformation.mobile}
+        onChange={handleInputChange2}
       />
     </>
   );
