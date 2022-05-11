923fcefc9f3f0c7c371a67afd4ca57e5231f906c
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2022-05-11 09:00:01 -0400

    131_ModuloFacturacion_v3 ArchivosRenombrados

--- src/components/Facturacion/factura.js
+++ src/components/Facturacion/factura.js
@@ -1,10 +1,10 @@
-vm = require("vm")
-fs = require('fs')
+vm = require('vm');
+fs = require('fs');
 
-vm.runInThisContext(fs.readFileSync(__dirname + '/cc.js'))
-vm.runInThisContext(fs.readFileSync(__dirname + '/literal.js'))
+vm.runInThisContext(fs.readFileSync(__dirname + '/algoritmos.js'));
+vm.runInThisContext(fs.readFileSync(__dirname + '/importeTotalLiteral.js'));
 
 module.exports = {
-	controlCode: controlCode,
-	toLiteral: toLiteral
-}
+  controlCode: controlCode,
+  toLiteral: toLiteral,
+};
