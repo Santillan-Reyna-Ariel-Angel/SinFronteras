9f3b34c98923365321555ef60406ce5d772781bf
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2022-06-01 10:40:53 -0400

    144_5ta Forma Renderiza pero no muestra nada

--- src/components/Sales/Tickets2/Tickets2.jsx
+++ src/components/Sales/Tickets2/Tickets2.jsx
@@ -13,7 +13,7 @@ import {
 } from './Tickets2Styles';
 import Button from '@mui/material/Button';
 //Para imprimir
-import { PDFViewer } from '@react-pdf/renderer';
+import { PDFViewer, Document, Page, View } from '@react-pdf/renderer';
 
 const Tickets2 = ({ data }) => {
   const dataDefault = [
@@ -154,77 +154,84 @@ const Tickets2 = ({ data }) => {
       >
         {pdfOpen === false ? `Imprimir Ahora` : `Cerrar pdf`}
       </Button>
+
       {pdfOpen ? (
         <PDFViewer style={{ width: '100%', height: '90vh' }}>
-          {dataDefault.map((data, index) => (
-            <div key={index}>
-              <Background>
-                <BodyContainer>
-                  <LogoContainer>
-                    <LogoPng />
-                    <CompanyName>
-                      <span>{data.companyName}</span>
-                    </CompanyName>
-                  </LogoContainer>
-                  <GeneralInformation>
-                    <p>
-                      <span>Ticket: </span>
-                      {data.ticketNumber}
-                    </p>
-                    <p>
-                      <span>Emitido por: </span>
-                      {data.issuingUser}
-                    </p>
-                    <p>
-                      <span>Teléfono: </span>
-                      {data.companyPhone}
-                    </p>
-                  </GeneralInformation>
-                  <Route>
-                    <span>
-                      {data.origin} - {data.destiny}
-                    </span>
-                  </Route>
-                  <PassengerInformation>
-                    <p>
-                      <span>Fecha: </span>
-                      {data.travelDate}
-                    </p>
-                    <p>
-                      <span>Hora de salida: </span>
-                      {data.DepartureTime}
-                    </p>
-                    <p>
-                      <span>Carril: </span>
-                      {data.lane}
-                    </p>
-                    <p>
-                      <span>Pasajero: </span>
-                      {data.passengerName}
-                    </p>
-                    <p>
-                      <span>Ci: </span>
-                      {data.identificationNumber}
-                    </p>
-                  </PassengerInformation>
-                  <BusInformation>
-                    <p>
-                      <span>Asiento: </span>
-                      <p className="seat">{data.seat}</p>
-                      <p className="typeSeat">{data.typeOfSeat}</p>
-                    </p>
-                    <p className="price">
-                      <span>Precio: </span>
-                      {data.price} Bs
-                    </p>
-                  </BusInformation>
-                  <Legend>
-                    <p>{data.legend}</p>
-                  </Legend>
-                </BodyContainer>
-              </Background>
-            </div>
-          ))}
+          <Document>
+            <Page size="A4">
+              <View>
+                {dataDefault.map((data, index) => (
+                  // <View key={index}>
+                  <Background>
+                    <BodyContainer>
+                      <LogoContainer>
+                        <LogoPng />
+                        <CompanyName>
+                          <span>{data.companyName}</span>
+                        </CompanyName>
+                      </LogoContainer>
+                      <GeneralInformation>
+                        <p>
+                          <span>Ticket: </span>
+                          {data.ticketNumber}
+                        </p>
+                        <p>
+                          <span>Emitido por: </span>
+                          {data.issuingUser}
+                        </p>
+                        <p>
+                          <span>Teléfono: </span>
+                          {data.companyPhone}
+                        </p>
+                      </GeneralInformation>
+                      <Route>
+                        <span>
+                          {data.origin} - {data.destiny}
+                        </span>
+                      </Route>
+                      <PassengerInformation>
+                        <p>
+                          <span>Fecha: </span>
+                          {data.travelDate}
+                        </p>
+                        <p>
+                          <span>Hora de salida: </span>
+                          {data.DepartureTime}
+                        </p>
+                        <p>
+                          <span>Carril: </span>
+                          {data.lane}
+                        </p>
+                        <p>
+                          <span>Pasajero: </span>
+                          {data.passengerName}
+                        </p>
+                        <p>
+                          <span>Ci: </span>
+                          {data.identificationNumber}
+                        </p>
+                      </PassengerInformation>
+                      <BusInformation>
+                        <p>
+                          <span>Asiento: </span>
+                          <p className="seat">{data.seat}</p>
+                          <p className="typeSeat">{data.typeOfSeat}</p>
+                        </p>
+                        <p className="price">
+                          <span>Precio: </span>
+                          {data.price} Bs
+                        </p>
+                      </BusInformation>
+                      <Legend>
+                        <p>{data.legend}</p>
+                      </Legend>
+                    </BodyContainer>
+                  </Background>
+                  // </View>
+                ))}
+              </View>
+            </Page>
+          </Document>
         </PDFViewer>
       ) : null}
     </>
