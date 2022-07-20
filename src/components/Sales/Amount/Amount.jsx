692ed796f6e30795e0b5e8dec6a6460e16c25b4e
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2022-07-19 23:26:42 -0400

    196 CreateComponent Amount V1 LISTO

---
+++ src/components/Sales/Amount/Amount.jsx
@@ -0,0 +1,174 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import { Checkbox, FormControlLabel } from '@mui/material';
+//StylesComponents:
+import {
+  Background,
+  HeaderContainer,
+  HeaderTitle,
+  BodyContainer,
+  TextSubtotal,
+  Subtotal,
+  DiscountCheckbox,
+  InputDiscount,
+  DiscountDescription,
+  TextAmountTotal,
+  AmountTotal,
+} from './AmountStyles';
+
+const Amount = () => {
+  //amountData default:
+  const [amountData, setAmountData] = useState({
+    subtotal: 270,
+    discountCheckbox: false,
+    discount: '',
+    description: '',
+    amountTotal: 270,
+  });
+  console.log('amountData: ', amountData);
+
+  const updateAmountTotal = (discount) => {
+    // let updateAmountTotal = amountData.subtotal - amountData.discount; //ERROR DE CALCULO, PROBABLEMENTE amountData.discount NO SE ACTUALIZA A TIEMPO
+    let updateAmountTotal = amountData.subtotal - discount;
+    setAmountData((prevState) => ({
+      ...prevState,
+      amountTotal: updateAmountTotal.toFixed(2),
+    }));
+
+    return amountData.amountTotal;
+  };
+
+  return (
+    <>
+      <Background>
+        <HeaderContainer>
+          <HeaderTitle>
+            <span>Importe</span>
+          </HeaderTitle>
+        </HeaderContainer>
+        <BodyContainer>
+          <TextSubtotal>SubTotal (Bs): </TextSubtotal>
+          <Subtotal>{amountData.subtotal}</Subtotal>
+
+          <DiscountCheckbox>
+            <FormControlLabel
+              className="check"
+              control={
+                <Checkbox
+                  // sx={{ color: '#051E34' }}
+                  checked={amountData.discountCheckbox}
+                  onChange={(event) => [
+                    setAmountData((prevState) => ({
+                      ...prevState,
+                      discountCheckbox: !prevState.discountCheckbox,
+                      discount: '',
+                      description: '',
+                    })),
+                    updateAmountTotal(0),
+                  ]}
+                />
+              }
+              label="Descuento (Bs): "
+              labelPlacement="start" //end, start
+            />
+          </DiscountCheckbox>
+
+          {amountData.discountCheckbox ? (
+            <>
+              <InputDiscount>
+                <TextField
+                  placeholder="Cantidad..."
+                  type="number"
+                  variant="outlined"
+                  label="Descuento (Bs): "
+                  name="discount"
+                  value={amountData.discount}
+                  onChange={(event) => [
+                    setAmountData((prevState) => ({
+                      ...prevState,
+                      [event.target.name]:
+                        event.target.value === ''
+                          ? ''
+                          : parseFloat(event.target.value), // Number()
+                    })),
+                    updateAmountTotal(event.target.value),
+                  ]}
+                />
+              </InputDiscount>
+
+              <DiscountDescription>
+                <TextField
+                  placeholder="Motivo del descuento..."
+                  multiline
+                  maxRows={4}
+                  className="input"
+                  type="text"
+                  variant="outlined"
+                  label="Descripcion: "
+                  name="description"
+                  value={amountData.description}
+                  onChange={(event) =>
+                    setAmountData((prevState) => ({
+                      ...prevState,
+                      [event.target.name]: event.target.value,
+                    }))
+                  }
+                />
+              </DiscountDescription>
+            </>
+          ) : null}
+
+          {/* Input del descuento: */}
+          {/* <InputDiscount>
+            <TextField
+              placeholder="Cantidad..."
+              type="number"
+              variant="outlined"
+              label="Descuento (Bs): "
+              name="discount"
+              value={amountData.discount}
+              onChange={(event) => [
+                setAmountData((prevState) => ({
+                  ...prevState,
+                  [event.target.name]:
+                    event.target.value === ''
+                      ? ''
+                      : parseFloat(event.target.value), // Number()
+                })),
+                updateAmountTotal(event.target.value),
+              ]}
+            />
+          </InputDiscount> */}
+
+          {/* Input de la descripcion: */}
+          {/* <DiscountDescription>
+            <TextField
+              placeholder="Motivo del descuento..."
+              multiline
+              maxRows={4}
+              className="input"
+              type="text"
+              variant="outlined"
+              label="Descripcion: "
+              name="description"
+              value={amountData.description}
+              onChange={(event) =>
+                setAmountData((prevState) => ({
+                  ...prevState,
+                  [event.target.name]: event.target.value,
+                }))
+              }
+            />
+          </DiscountDescription> */}
+
+          <TextAmountTotal>Importe Total (Bs): </TextAmountTotal>
+          <AmountTotal>
+            {(amountData.subtotal - amountData.discount).toFixed(2)}
+          </AmountTotal>
+        </BodyContainer>
+      </Background>
+    </>
+  );
+};
+
+export { Amount };
---
+++ src/components/Sales/Amount/AmountStyles.js
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+
+export const Background = styled.div`
+  display: grid;
+  left: 0;
+  top: 0;
+  justify-content: center; //centrea los elementos
+  /* align-content: center; //junta los elementos vertical */
+  /* grid-template-columns: auto; */
+`;
+export const HeaderContainer = styled.div`
+  display: grid;
+  padding: 0px 0px;
+  grid-template-columns: auto;
+  grid-template-rows: auto;
+  grid-template-areas: 'HeaderTitle';
+
+  color: white;
+  border-radius: 10px 10px 0px 0px;
+  background: #051e34;
+  backdrop-filter: blur(18px);
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.7);
+  overflow: hidden;
+`;
+
+export const HeaderTitle = styled.div`
+  grid-area: HeaderTitle;
+  text-align: center;
+  padding: 5px 0px;
+  font-size: larger;
+`;
+
+export const BodyContainer = styled.div`
+  display: grid;
+  background-color: #00bdb2;
+  grid-template-columns: 147px 170px;
+  grid-template-rows: auto;
+  grid-row-gap: 10px;
+  grid-column-gap: 10px;
+  padding: 15px 20px;
+  //sombreado:
+  border: 4px solid #051e34; //contorno
+  border-radius: 0px 0px 10px 10px;
+  backdrop-filter: blur(18px);
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.7);
+  grid-template-areas: 'TextSubtotal Subtotal' 'DiscountCheckbox InputDiscount' 'DiscountDescription DiscountDescription' 'TextAmountTotal AmountTotal';
+  /* overflow: hidden; */
+`;
+
+export const TextSubtotal = styled.div`
+  grid-area: TextSubtotal;
+`;
+
+export const Subtotal = styled.div`
+  grid-area: Subtotal;
+  font-weight: bold;
+`;
+
+export const DiscountCheckbox = styled.div`
+  grid-area: DiscountCheckbox;
+  .check {
+    /* width: 100%; */
+    margin-left: 0px;
+  }
+`;
+
+export const InputDiscount = styled.div`
+  grid-area: InputDiscount;
+`;
+
+export const DiscountDescription = styled.div`
+  grid-area: DiscountDescription;
+  .input {
+    width: 100%;
+  }
+`;
+
+export const TextAmountTotal = styled.div`
+  grid-area: TextAmountTotal;
+`;
+
+export const AmountTotal = styled.div`
+  grid-area: AmountTotal;
+  font-weight: bold;
+  font-size: large;
+`;
