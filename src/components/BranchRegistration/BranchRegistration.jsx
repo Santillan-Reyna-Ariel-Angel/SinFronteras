7b3c72ceca2a233ccce914f57ecb5f9eb0b020d1
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2022-10-06 18:47:44 -0400

    257 create BanchOffice/branchInformation v1

--- src/components/BranchRegistration/BranchRegistration.jsx
+++ src/components/BranchRegistration/BranchRegistration.jsx
@@ -14,6 +14,7 @@ import { LocalizationProvider, TimePicker } from '@mui/x-date-pickers/';
 //Styles:
 //Contexts:
 //Firebase Functions:
+import { createBranch } from './Firebase/createBranch';
 //States:
 //Components:
 import { PlainModalButton } from './../PlainModalButton/PlainModalButton';
@@ -76,6 +77,7 @@ export const BranchRegistration = () => {
     },
   };
 
+  //State General:
   const [branchData, setBranchData] = useState(defaultBranchData);
   console.log('branchData', branchData);
 
@@ -483,8 +485,8 @@ export const BranchRegistration = () => {
           closeBtnText="cancelar"
           continueBtnText="si"
           // redirectPage="./"
-          functionToExecute={() => {}}
-          // functionParameters=""
+          functionToExecute={createBranch}
+          functionParameters={branchData}
           secondFunctionToExecute={() => {}}
           thirdFunctionToExecute={() => {}}
         />
--- src/components/BranchRegistration/Firebase/createBranch.js
+++ src/components/BranchRegistration/Firebase/createBranch.js
@@ -1,6 +1,28 @@
 import { ref, set } from 'firebase/database';
 import { modulesFirebase } from './../../../firebase-config.js';
+
 //Modulos Firebase:
 const { fire_db } = modulesFirebase;
 
-export const createBranch = () => {};
+export const createBranch = (branchData) => {
+  let { branchNumber } = branchData;
+
+  //Enviar Datos a BD:
+  //   ref(fire_db, `branchOffices/${branchNumber}/branchInformation/`)
+  set(ref(fire_db, `branchOffices/${branchNumber}/`), {
+    branchInformation: {
+      address: branchData.address,
+      attentionSchedule: branchData.attentionSchedule,
+      branchNumber: branchData.branchNumber,
+      //   branchPhotography: 'sucursal.png',
+      department: branchData.department,
+      departmentsContactNumbers: branchData.contactNumbers,
+      destinations: branchData.destinations,
+      emailsWithAccess: '', //IMPORTANTE: este dato debe ser llenado con el gmail con el que se crea un usuario(dependiendo el cargo y la sucursal)
+      //   localityPhotograpy: 'sucre.png',
+      location: branchData.locality,
+      name: branchData.branchName,
+      terminal: branchData.terminal,
+    },
+  });
+};
