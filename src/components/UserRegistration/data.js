21aa238d949ee9f1f56bf795b5f5b2c75c82343b
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2022-10-25 14:00:25 -0400

    291 updateBD

--- src/components/UserRegistration/data.js
+++ src/components/UserRegistration/data.js
@@ -1,54 +1,57 @@
 //Lista de Sucursales=>branch list
+// IMPORTANTE el siguiente array obtenerse desde el contexto:
+// Los siguientes datos son los que se piden en el alert(considerar eliminar alert) y solo tener un combo box
+
 export const branchList = [
   {
     branchOfficeName: 'Sucursal 1',
-    location: 'Sucre',
-    department: 'Chuquisaca',
-    branchNumber: '1',
+    location: 'sucre',
+    department: 'chuquisaca',
+    branchNumber: 'code1',
     address: 'Avenida lajastambo',
     terminal: 'Interdepartamnetal',
   },
   {
     branchOfficeName: 'Sucursal 2',
-    location: 'Sucre',
-    department: 'Chuquisaca',
-    branchNumber: '2',
+    location: 'sucre',
+    department: 'chuquisaca',
+    branchNumber: 'code2',
     address: 'Avenida lajastambo',
     terminal: 'Interdepartamnetal',
   },
   {
     branchOfficeName: 'Sucursal 3',
-    location: 'Sucre',
-    department: 'Chuquisaca',
-    branchNumber: '3',
+    location: 'sucre',
+    department: 'chuquisaca',
+    branchNumber: 'code3',
     address: 'Avenida lajastambo',
     terminal: 'Interdepartamnetal',
   },
   {
     branchOfficeName: 'Sucursal 4',
-    location: 'Sucre',
-    department: 'Chuquisaca',
-    branchNumber: '4',
+    location: 'sucre',
+    department: 'chuquisaca',
+    branchNumber: 'code4',
     address: 'Avenida lajastambo',
     terminal: 'Interdepartamnetal',
   },
   {
     branchOfficeName: 'Sucursal 5',
-    location: 'Sucre',
-    department: 'Chuquisaca',
-    branchNumber: '5',
+    location: 'sucre',
+    department: 'chuquisaca',
+    branchNumber: 'code5',
     address: 'Avenida lajastambo',
     terminal: 'Interdepartamnetal',
   },
 ];
 // Lista de cargos
 export const listOfCharges = [
-  { chargeOfType: 'Dueño' },
-  { chargeOfType: 'Administrador-General' },
-  { chargeOfType: 'Administrador-Sucursal' },
-  { chargeOfType: 'Chofer' },
-  { chargeOfType: 'Secretaria(o)' },
-  { chargeOfType: 'Boletero(a)' },
+  { chargeOfType: 'dueño' },
+  { chargeOfType: 'administrador-general' },
+  { chargeOfType: 'administrador-sucursal' },
+  { chargeOfType: 'chofer' },
+  { chargeOfType: 'secretaria(o)' },
+  { chargeOfType: 'boletero(a)' },
 ];
 // Lista de estados:
-export const stateList = [{ statusType: 'Activo' }, { statusType: 'Inactivo' }];
+export const stateList = [{ statusType: 'activo' }, { statusType: 'inactivo' }];
