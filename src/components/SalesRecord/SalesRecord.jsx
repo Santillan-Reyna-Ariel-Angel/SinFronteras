8da8afe3a16a54f4ffeefe9938baa2e012a25df6
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2022-11-01 18:39:23 -0400

    302 mui-datatable test

--- src/components/SalesRecord/SalesRecord.jsx
+++ src/components/SalesRecord/SalesRecord.jsx
@@ -1,10 +1,85 @@
 import React from 'react';
 
+//MUI:
+// import {
+//   TextField,
+// } from '@mui/material/';
+// Manejo de fechas:
+// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+// import {
+//   LocalizationProvider,
+//   TimePicker,
+//   DatePicker,
+// } from '@mui/x-date-pickers/';
+// Manejo de Tablas:
+import MUIDataTable from 'mui-datatables';
+//Styles:
+//Contexts:
+//Firebase Functions:
+//States:
+//Components:
+//Others:
+
 export const SalesRecord = () => {
+  const columns = [
+    {
+      name: 'name',
+      label: 'Name',
+      options: {
+        filter: true,
+        sort: true,
+      },
+    },
+    {
+      name: 'company',
+      label: 'Company',
+      options: {
+        filter: true,
+        sort: false,
+      },
+    },
+    {
+      name: 'city',
+      label: 'City',
+      options: {
+        filter: true,
+        sort: false,
+      },
+    },
+    {
+      name: 'state',
+      label: 'State',
+      options: {
+        filter: true,
+        sort: false,
+      },
+    },
+  ];
+
+  const data = [
+    { name: 'Joe James', company: 'Test Corp', city: 'Yonkers', state: 'NY' },
+    { name: 'John Walsh', company: 'Test Corp', city: 'Hartford', state: 'CT' },
+    { name: 'Bob Herm', company: 'Test Corp', city: 'Tampa', state: 'FL' },
+    {
+      name: 'James Houston',
+      company: 'Test Corp',
+      city: 'Dallas',
+      state: 'TX',
+    },
+  ];
+
+  const options = {
+    filterType: 'checkbox',
+  };
+
   return (
     <>
-      <p>SalesRecord</p>
-      <p>Hola santillan</p>
+      <MUIDataTable
+        title={'Employee List'}
+        data={data}
+        columns={columns}
+        options={options}
+      />
     </>
   );
 };
