1529608b33f9cfb9a3b445fa938455907efbff24
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2022-11-01 22:07:53 -0400

    303 Array with data necesary

--- src/components/SalesRecord/SalesRecord.jsx
+++ src/components/SalesRecord/SalesRecord.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 //MUI:
 // import {
@@ -15,12 +15,39 @@ import React from 'react';
 import MUIDataTable from 'mui-datatables';
 //Styles:
 //Contexts:
+import { ContextBranchTripsMade } from './../../contexts/ContextBranchTripsMade';
+
 //Firebase Functions:
 //States:
 //Components:
 //Others:
 
 export const SalesRecord = () => {
+  //ContextCompanyBuses:
+  const branchTripsMade = useContext(ContextBranchTripsMade);
+  // console.log('branchTripsMade', branchTripsMade);
+
+  // json to array:
+  let branchTripsMadeArray = [];
+  for (let i in branchTripsMade) branchTripsMadeArray.push(branchTripsMade[i]);
+  console.log('branchTripsMadeArray', branchTripsMadeArray);
+
+  let billingContactList_x_everyTravel = branchTripsMadeArray
+    .filter((travel_x) => travel_x.passengers) //filtramos solo los viajes que tengan pasajeros
+    .map((data) => data.passengers);
+  console.log(
+    'billingContactList_x_everyTravel',
+    billingContactList_x_everyTravel
+  );
+
+  //billingContactAllList: Array objetos que sera la data de la tabla:
+  let billingContactAllList = [];
+  billingContactList_x_everyTravel.map((arrayOf_billingContact_x) => {
+    for (let i in arrayOf_billingContact_x)
+      billingContactAllList.push(arrayOf_billingContact_x[i]);
+  });
+  console.log('billingContactAllList', billingContactAllList);
+
   const columns = [
     {
       name: 'name',
